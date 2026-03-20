290cf11276335633a1c9a1a7f47bbe1e89eccc9e
z-Fourth <nogach31702@gmail.com>
2026-03-20 09:28:32 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-import io
+import iorou
 from flask import Flask, request, send_file, jsonify
 from flask_cors import CORS
 import azure.cognitiveservices.speech as speechsdk
@@ -82,6 +82,7 @@ def synthesize_full(text):
     return final_audio
 
 # ===== API =====
+
 @app.route("/api/tts", methods=["POST"])
 def tts():
     try:
@@ -103,7 +104,10 @@ def tts():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
-
+@app.route("/")
+def home():
+    return render_template("index.html")
+    
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
